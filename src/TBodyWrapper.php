ba20b4d8b20bfe752498af31c07d6a71ae208574
Andreas Hennings <andreas@dqxtech.net>
2014-07-28 22:32:02 +0200

    Split out stuff from Table into TBodyWrapper, and introduce TableSectionInterface

---
+++ src/TBodyWrapper.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Donquixote\Cellbrush;
+
+/**
+ * Wrapper/decorator for a tbody element.
+ */
+class TBodyWrapper implements TableSectionInterface {
+
+  /**
+   * @var TableSection
+   */
+  private $tbody;
+
+  /**
+   * The constructor.
+   *
+   * @param \Donquixote\Cellbrush\TableSection $tbody
+   */
+  function __construct(TableSection $tbody) {
+    $this->tbody = $tbody;
+  }
+
+  /**
+   * @return TableSection
+   */
+  function tbody() {
+    return $this->tbody;
+  }
+
+  /**
+   * @param string $colName
+   *
+   * @return SectionColHandle
+   */
+  function colHandle($colName) {
+    return $this->tbody->colHandle($colName);
+  }
+
+  /**
+   * @param string $rowName
+   *
+   * @return $this
+   * @throws \Exception
+   */
+  function addRowName($rowName) {
+    $this->tbody->addRowName($rowName);
+    return $this;
+  }
+
+  /**
+   * @param string[] $rowNames
+   *
+   * @return $this
+   */
+  function addRowNames(array $rowNames) {
+    $this->tbody->addRowNames($rowNames);
+    return $this;
+  }
+
+  /**
+   * @param string $groupName
+   * @param string[] $rowNameSuffixes
+   *
+   * @return $this
+   * @throws \Exception
+   */
+  function addRowGroup($groupName, array $rowNameSuffixes) {
+    $this->tbody->addRowGroup($groupName, $rowNameSuffixes);
+    return $this;
+  }
+
+  /**
+   * @param string $rowName
+   *
+   * @return RowHandle
+   * @throws \Exception
+   */
+  public function rowHandle($rowName) {
+    return $this->tbody->rowHandle($rowName);
+  }
+
+  /**
+   * Adds a row and returns the row handle.
+   * This is a hybrid of addRowName() and rowHandle().
+   *
+   * @param $rowName
+   *
+   * @return RowHandle
+   * @throws \Exception
+   */
+  public function addRow($rowName) {
+    return $this->tbody->addRow($rowName);
+  }
+
+  /**
+   * @param string $rowName
+   * @param string $class
+   *
+   * @return $this
+   */
+  public function addRowClass($rowName, $class) {
+    $this->tbody->addRowClass($rowName, $class);
+    return $this;
+  }
+
+  /**
+   * @param string[] $rowClasses
+   *   Format: $[$rowName] = $class
+   *
+   * @return $this
+   */
+  public function addRowClasses(array $rowClasses) {
+    $this->tbody->addRowClasses($rowClasses);
+    return $this;
+  }
+
+  /**
+   * @param string[] $striping
+   *   Classes for striping. E.g. ['odd', 'even'], or '['1st', '2nd', '3rd'].
+   *
+   * @return $this
+   */
+  public function addRowStriping(array $striping = ['odd', 'even']) {
+    $this->tbody->addRowStriping($striping);
+    return $this;
+  }
+
+  /**
+   * @param string|string[] $rowName
+   *   Row name, group or range.
+   * @param string|string[] $colName
+   *   Column name, group or range.
+   * @param string $content
+   *   HTML cell content.
+   *
+   * @return $this
+   * @throws \Exception
+   */
+  function td($rowName, $colName, $content) {
+    $this->tbody->td($rowName, $colName, $content);
+    return $this;
+  }
+
+  /**
+   * @param string|string[] $rowName
+   *   Row name, group or range.
+   * @param string|string[] $colName
+   *   Column name, group or range.
+   * @param string $content
+   *   HTML cell content.
+   *
+   * @return $this
+   * @throws \Exception
+   */
+  function th($rowName, $colName, $content) {
+    $this->tbody->th($rowName, $colName, $content);
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  function render() {
+    return $this->tbody->render('tbody');
+  }
+
+}
--- src/Table.php
+++ src/Table.php
@@ -2,7 +2,7 @@
 
 namespace Donquixote\Cellbrush;
 
-class Table {
+class Table extends TBodyWrapper {
 
   /**
    * @var TableColumns
@@ -13,11 +13,6 @@ class Table {
    * @var TableSection
    */
   private $thead;
-
-  /**
-   * @var TableSection
-   */
-  private $tbody;
 
   /**
    * Additional tbody sections.
@@ -38,7 +33,7 @@ class Table {
   function __construct() {
     $this->columns = new TableColumns();
     $this->thead = new TableSection($this->columns);
-    $this->tbody = new TableSection($this->columns);
+    parent::__construct(new TableSection($this->columns));
     $this->tfoot = new TableSection($this->columns);
   }
 
@@ -85,16 +80,6 @@ class Table {
 
   /**
    * @param string $colName
-   *
-   * @return SectionColHandle
-   */
-  function colHandle($colName) {
-    $this->columns->verifyColName($colName);
-    return new TableColHandle($this, $this->tbody, $colName);
-  }
-
-  /**
-   * @param string $colName
    * @param string $class
    *
    * @return $this
@@ -130,7 +115,7 @@ class Table {
    */
   function tbody($name = NULL) {
     if (!isset($name)) {
-      return $this->tbody;
+      return parent::tbody();
     }
     return isset($this->tbodies[$name])
       ? $this->tbodies[$name]
@@ -156,127 +141,6 @@ class Table {
   }
 
   /**
-   * @param string $rowName
-   *
-   * @return $this
-   * @throws \Exception
-   */
-  function addRowName($rowName) {
-    $this->tbody->addRowName($rowName);
-    return $this;
-  }
-
-  /**
-   * @param string[] $rowNames
-   *
-   * @return $this
-   */
-  function addRowNames(array $rowNames) {
-    $this->tbody->addRowNames($rowNames);
-    return $this;
-  }
-
-  /**
-   * @param string $groupName
-   * @param string[] $rowNameSuffixes
-   *
-   * @return $this
-   * @throws \Exception
-   */
-  function addRowGroup($groupName, $rowNameSuffixes) {
-    $this->tbody->addRowGroup($groupName, $rowNameSuffixes);
-    return $this;
-  }
-
-  /**
-   * @param string $rowName
-   *
-   * @return RowHandle
-   * @throws \Exception
-   */
-  public function rowHandle($rowName) {
-    return $this->tbody->rowHandle($rowName);
-  }
-
-  /**
-   * Adds a row and returns the row handle.
-   * This is a hybrid of addRowName() and rowHandle().
-   *
-   * @param $rowName
-   *
-   * @return RowHandle
-   * @throws \Exception
-   */
-  public function addRow($rowName) {
-    return $this->tbody->addRow($rowName);
-  }
-
-  /**
-   * @param string $rowName
-   * @param string $class
-   *
-   * @return $this
-   */
-  public function addRowClass($rowName, $class) {
-    $this->tbody->addRowClass($rowName, $class);
-    return $this;
-  }
-
-  /**
-   * @param string[] $rowClasses
-   *   Format: $[$rowName] = $class
-   *
-   * @return $this
-   */
-  public function addRowClasses(array $rowClasses) {
-    $this->tbody->addRowClasses($rowClasses);
-    return $this;
-  }
-
-  /**
-   * @param string[] $striping
-   *   Classes for striping. E.g. ['odd', 'even'], or '['1st', '2nd', '3rd'].
-   *
-   * @return $this
-   */
-  public function addRowStriping($striping = ['odd', 'even']) {
-    $this->tbody->addRowStriping($striping);
-    return $this;
-  }
-
-  /**
-   * @param string|string[] $rowName
-   *   Row name, group or range.
-   * @param string|string[] $colName
-   *   Column name, group or range.
-   * @param string $content
-   *   HTML cell content.
-   *
-   * @return $this
-   * @throws \Exception
-   */
-  function td($rowName, $colName, $content) {
-    $this->tbody->td($rowName, $colName, $content);
-    return $this;
-  }
-
-  /**
-   * @param string|string[] $rowName
-   *   Row name, group or range.
-   * @param string|string[] $colName
-   *   Column name, group or range.
-   * @param string $content
-   *   HTML cell content.
-   *
-   * @return $this
-   * @throws \Exception
-   */
-  function th($rowName, $colName, $content) {
-    $this->tbody->th($rowName, $colName, $content);
-    return $this;
-  }
-
-  /**
    * @return string
    *   Rendered table html.
    */
@@ -284,7 +148,7 @@ class Table {
     $html = '';
     $html .= $this->thead->render('thead');
     $html .= $this->tfoot->render('tfoot');
-    $html .= $this->tbody->render('tbody');
+    $html .= parent::render();
     foreach ($this->tbodies as $tbody) {
       $html .= $tbody->render('tbody');
     }
