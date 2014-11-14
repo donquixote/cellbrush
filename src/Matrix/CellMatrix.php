9f5a387107cdf36b33cd6a48893dc485e07b191b
Andreas Hennings <andreas@dqxtech.net>
2014-11-14 15:13:44 +0100

    Remove all brushy stuff.

--- src/Matrix/CellMatrix.php
+++ src/Matrix/CellMatrix.php
@@ -48,19 +48,6 @@ class CellMatrix {
   }
 
   /**
-   * @param BrushInterface $brush
-   * @param CellInterface $cell
-   */
-  public function brushAddCell(BrushInterface $brush, CellInterface $cell) {
-    if ($brush->hasRange()) {
-      $this->brushPaintShadow($brush);
-      $cell = $cell->setRowspan($brush->nRows())->setColspan($brush->nCols());
-    }
-
-    $this->cells[$brush->iRow()][$brush->iCol()] = $cell;
-  }
-
-  /**
    * @param int $iRow
    * @param int $iCol
    * @param \Donquixote\Cellbrush\Cell\CellInterface $cell
@@ -73,31 +60,6 @@ class CellMatrix {
       // @todo Check for collision.
     }
     $this->cells[$iRow][$iCol] = $cell;
-  }
-
-  /**
-   * @param BrushInterface $brush
-   *   The current area occupied by the cell.
-   *
-   * @return RangedBrush
-   *   The area occupied by the cell after the operation.
-   */
-  public function brushCellGrowRight(BrushInterface $brush) {
-    $cell =& $this->brushRequireCell($brush);
-    $colspan = $cell->getColspan();
-    $nextBrush = $brush;
-    while ($nextBrush = $nextBrush->getNext()) {
-      $nextBrush->iColSup();
-      if (!$this->brushIsFree($nextBrush)) {
-        break;
-      }
-      $this->brushPaintShadow($nextBrush);
-      $colspan += $nextBrush->nCols();
-    }
-    $cell = $cell->setColspan($colspan);
-
-    // Return a brush representing the new area occupied by the cell.
-    return $brush->setColspan($colspan);
   }
 
   /**
@@ -133,60 +95,6 @@ class CellMatrix {
   }
 
   /**
-   * Finds a cell that matches exactly the brush, or throws an exception.
-   *
-   * @param BrushInterface $brush
-   *
-   * @return CellInterface
-   */
-  public function &brushRequireCell(BrushInterface $brush) {
-    $iCol = $brush->iCol();
-    $iRow = $brush->iRow();
-    if (!isset($this->cells[$iRow][$iCol])) {
-      throw new \RuntimeException('Illegal growing.');
-    }
-    $cell =& $this->cells[$iRow][$iCol];
-    if ($cell instanceof PlaceholderCell) {
-      throw new \RuntimeException('Illegal growing.');
-    }
-    if ($cell instanceof ShadowCell) {
-      throw new \RuntimeException('Illegal growing.');
-    }
-    if ($cell->getRowspan() !== $brush->nRows()) {
-      throw new \RuntimeException('Illegal growing.');
-    }
-    if ($cell->getColspan() !== $brush->nCols()) {
-      throw new \RuntimeException('Illegal growing.');
-    }
-    return $cell;
-  }
-
-  /**
-   * @param BrushInterface $brush
-   *
-   * @return bool
-   *   true, if the brush area is free to add new cells.
-   */
-  public function brushIsFree(BrushInterface $brush) {
-    $colIndices = $brush->getColIndices();
-    foreach ($brush->getRowIndices() as $iRow) {
-      if (!isset($this->cells[$iRow])) {
-        return false;
-      }
-      $rowCells = $this->cells[$iRow];
-      foreach ($colIndices as $iCol) {
-        if (!isset($rowCells[$iCol])) {
-          return false;
-        }
-        if (!$rowCells[$iCol] instanceof PlaceholderCell) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  /**
    * @param int $iRowMin
    * @param int $iColMin
    * @param int $iRowSup
@@ -211,33 +119,6 @@ class CellMatrix {
       }
     }
     return true;
-  }
-
-  /**
-   * Fills an area with shadow cells.
-   *
-   * This has to be private, because it breaks the matrix invariant of being a
-   * valid html table.
-   *
-   * @param BrushInterface $brush
-   */
-  private function brushPaintShadow(BrushInterface $brush) {
-    $colIndices = $brush->getColIndices();
-    foreach ($brush->getRowIndices() as $rowIndex) {
-      if (!Isset($this->cells[$rowIndex])) {
-        throw new \RuntimeException('Illegal row index');
-      }
-      $rowCells =& $this->cells[$rowIndex];
-      foreach ($colIndices as $colIndex) {
-        if (!isset($rowCells[$colIndex])) {
-          throw new \RuntimeException('Illegal col index');
-        }
-        if (!$rowCells[$colIndex] instanceof PlaceholderCell) {
-          throw new \RuntimeException('Illegal position');
-        }
-        $rowCells[$colIndex] = $this->shadowCell;
-      }
-    }
   }
 
   /**
--- tests/src/CellMatrixTest.php
+++ tests/src/CellMatrixTest.php
@@ -2,12 +2,8 @@
 
 namespace Donquixote\Cellbrush\Tests;
 
-use Donquixote\Cellbrush\Axis\SimpleRange;
 use Donquixote\Cellbrush\Cell\Cell;
-use Donquixote\Cellbrush\Cell\PlaceholderCell;
-use Donquixote\Cellbrush\Cell\ShadowCell;
 use Donquixote\Cellbrush\Matrix\CellMatrix;
-use Donquixote\Cellbrush\Matrix\RangedBrush;
 
 class CellMatrixTest extends \PHPUnit_Framework_TestCase {
 
@@ -92,30 +88,6 @@ EOT;
 EOT;
 
     $this->assertEquals($expected, $cellMatrix->renderAsTable());
-  }
-
-  function testCellMatrixWithBrush() {
-    $cellMatrix = CellMatrix::create(1, 2);
-    $rowRange = new SimpleRange(0, 1);
-    $colRange = new SimpleRange(0, 1);
-    $brush = new RangedBrush($rowRange, $colRange);
-    $cell = new Cell('td', '0..1');
-
-    $cellMatrix->brushAddCell($brush, $cell);
-    $this->assertEquals(
-      [[
-        $cell,
-        new PlaceholderCell(),
-      ]],
-      $cellMatrix->getCells());
-
-    $cellMatrix->brushCellGrowRight($brush);
-    $this->assertEquals(
-      [[
-        $cell->setColspan(2),
-        new ShadowCell(),
-      ]],
-      $cellMatrix->getCells());
   }
 
 }
