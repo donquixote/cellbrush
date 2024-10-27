d1ce987cb23592ea00bea08d67a323f14a607dd1
Andreas Hennings <andreas@dqxtech.net>
2024-10-27 21:38:02 +0100

    Issue #13: Let TableSectionInterface extend ColumnClassesInterface.

--- src/TSection/TableSectionInterface.php
+++ src/TSection/TableSectionInterface.php
@@ -2,11 +2,12 @@
 
 namespace Donquixote\Cellbrush\TSection;
 
+use Donquixote\Cellbrush\Columns\ColumnClassesInterface;
 use Donquixote\Cellbrush\Html\MutableAttributesInterface;
 
 /**
  * Interface for a table section element.
  */
-interface TableSectionInterface extends MutableAttributesInterface, TableSectionStructureInterface {
+interface TableSectionInterface extends ColumnClassesInterface, MutableAttributesInterface, TableSectionStructureInterface {
 
 }
